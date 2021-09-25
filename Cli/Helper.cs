5bad79c1362236b8753692f79e4c810d8e28ec33
Andy <aledesmacc@gmail.com>
2021-09-25 18:39:27 -0500

    NET 5. CLI Basis
    - Helper methods for CLI.
    - ConsoleApp1 deleted.
    - ConsoleApp2 framework is net5.0.
    - Deleted backup commands (made by Pablo).

---
+++ Cli/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cli {
+    internal static class Helper {
+        // ASSUMING IT COULDN'T BE 2 OPTIONS STARTING WITH THE SAME LETTER, I.E.
+        // --help --home
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="optionLetter"></param>
+        /// <param name="default">Value to return if option is not found.</param>
+        /// <returns></returns>
+        internal static string ValueOfOption(this string[] args, char optionLetter, string @default = default) {
+            for (uint i = 0; i < args.Length; i++) {
+                if (IsOption(args[i], optionLetter)) {// option found
+                    if (i + 1 != args.Length) {
+                        return args[i + 1];
+                    }
+                    throw new InvalidOptionOrValueException();// no provided value
+                }
+            }
+            return @default;
+        }
+
+        internal static bool ExistsOption(this string[] args, char optionLetter) {
+            return args.Any(a => IsOption(a, optionLetter));
+        }
+
+        private static bool IsOption(string arg, char optionLetter) {
+            var regex = new Regex(@"\-?\-" + optionLetter);
+            var match = regex.Match(arg);
+
+            return match.Success && match.Index == 0;
+        }
+    }
+}
---
+++ Cli/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cli {
+    class Program {
+        static void Main(string[] args) {
+            var showHelp = args.ExistsOption('h');
+            var cliName = "ivns";
+            var im = "\t";  // intermediate margin
+
+            try {
+                var @struct = args.ValueOfOption('s', "list");
+                Console.WriteLine($"Usando {StructureInfo(@struct)}...");
+
+            } catch (InvalidOptionOrValueException) {
+
+                showHelp = true;
+            }
+
+            if (showHelp) {
+                Console.WriteLine(
+                    $"\nUso: {cliName} archivo_del_problema [opciones]\n\n" +
+                    "Opciones:\n" +
+                    $"-h, --help{im} Muestra esta ayuda.\n" +
+                    $"-s, --structure{im} Estructura de datos a utilizar para almacenar los clientes en el algoritmo. " +
+                        "Los posibles valores son:\n" +
+                        "\t list\t para emplear la lista de C# (valor por defecto),\n" +
+                        "\t avl\t para emplear un AVL, o\n" +
+                        "\t skip\t para emplear una Skip Lists."
+                );
+            }
+
+            //while (true) {
+            //    var opt = Console.ReadLine()[0];
+            //    Console.WriteLine(args.ValueOfOption(opt));
+            //    Console.WriteLine(args.ExistsOption(opt));
+            //}
+        }
+
+        private static string StructureInfo(string @struct) {
+            return @struct switch {
+                "avl" => "AVL",
+                "skip" => "Skip Lists",
+                "list" => "lista de C#",
+                _ => throw new InvalidOptionOrValueException()
+            };
+        }
+    }
+}
